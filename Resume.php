52657fc121add07e77271abcadc5153dc7f0428b
= <luisfmanzanoa@gmail.com>
2020-04-21 13:54:27 -0400

    primer commit a pesar de que ya hay varias cosas hechas

---
+++ Resume.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<!-- saved from url=(0115)https://static.platzi.com/media/tmp/class-files/git/curso-introduccion-php/curso-introduccion-php-master/index.html -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <!-- Required meta tags -->
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="./Resume_files/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <link rel="stylesheet" href="./Resume_files/style.css">
+
+  <title>Resume</title>
+</head>
+<?php 
+
+$name = "Luis";
+$apellido = "Manzano";
+
+$jobs = [
+    [
+      'title' => 'PHP Developer',
+      'description' => 'This is an awesome job' 
+    ],
+    [
+      'title' => 'Javascript Developer',
+      'description' => 'Este es aun mejor que el anterior'
+    ]
+];
+
+function printJob($job) {
+  echo '<li class = "work-position">';
+  echo '<h5>' .  $jobs[$idx]['title'] . '</h5>';
+  echo '<p>' .  $jobs[$idx]['description'] . '</p>';
+  echo '<strong>Achievements:</strong>';
+  echo '<ul>';
+  echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+  echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+  echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+  echo '</ul>';
+  echo '</li>';
+}
+
+
+
+?>
+<body>
+  <div class="container">
+    <div id="resume-header" class="row">
+      <div class="col-3">
+        <img id="profile-picture" src="./Resume_files/saved_resource" alt="">
+      </div>
+      <div class="col">
+        <h1><?php echo "$name $apellido" ?></h1>
+        <h2>PHP Developer</h2>
+        <ul>
+          <li>Mail: luisfer@mail.com</li>
+          <li>Phone: 1234567890</li>
+          <li>LinkedIn: https://linkedin.com</li>
+          <li>Twitter: @luisfmanzanoa</li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h2 class="border-bottom-gray">Carrer Summary</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div>
+          <h3 class="border-bottom-gray">Work Experience</h3>
+          <ul>
+            
+          <?php
+
+                for($idx = 0; $idx < count($jobs); $idx++){
+              echo '<li class = "work-position">';
+              echo '<h5>' .  $jobs[$idx]['title'] . '</h5>';
+              echo '<p>' .  $jobs[$idx]['description'] . '</p>';
+              echo '<strong>Achievements:</strong>';
+              echo '<ul>';
+              echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+              echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+              echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+              echo '</ul>';
+              echo '</li>';
+                }
+                
+                ?>
+          </ul>
+        </div>
+        <div>
+            <h3 class="border-bottom-gray">Projects</h3>
+            <div class="project">
+                <h5>Project X</h5>
+                <div class="row">
+                    <div class="col-3">
+                        <img id="profile-picture" src="./Resume_files/saved_resource" alt="">
+                      </div>
+                      <div class="col">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
+                        <strong>Technologies used:</strong>
+                        <span class="badge badge-secondary">PHP</span>
+                        <span class="badge badge-secondary">HTML</span>
+                        <span class="badge badge-secondary">CSS</span>
+                      </div>
+                </div>
+            </div>
+            <div class="project">
+                <h5>Project X</h5>
+                <div class="row">
+                    <div class="col-3">
+                        <img id="profile-picture" src="./Resume_files/saved_resource" alt="">
+                      </div>
+                      <div class="col">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
+                        <strong>Technologies used:</strong>
+                        <span class="badge badge-secondary">PHP</span>
+                        <span class="badge badge-secondary">HTML</span>
+                        <span class="badge badge-secondary">CSS</span>
+                      </div>
+                </div>
+            </div>
+          </div>
+      </div>
+      <div class="col-3">
+        <h3 class="border-bottom-gray">Skills &amp; Tools</h3>
+        <h4>Backend</h4>
+        <ul>
+          <li>PHP</li>
+        </ul>
+        <h4>Frontend</h4>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+        </ul>
+        <h4>Frameworks</h4>
+        <ul>
+          <li>Laravel</li>
+          <li>Bootstrap</li>
+        </ul>
+        <h3 class="border-bottom-gray">Languages</h3>
+        <ul>
+          <li>Spanish</li>
+          <li>English</li>
+        </ul>
+      </div>
+    </div>
+    <div id="resume-footer" class="row">
+      <div class="col">
+          Designed by @hectorbenitez
+      </div>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="./Resume_files/jquery-3.3.1.slim.min.js.download" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="./Resume_files/popper.min.js.download" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="./Resume_files/bootstrap.min.js.download" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+
+
+</body></html>
